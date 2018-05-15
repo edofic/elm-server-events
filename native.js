c71dcc6aaaea5d028fdfa0e5581cb5acfd7b524b
Andraz Bajt <andraz@bajt.me>
2018-05-15 07:34:21 +0200

    Log msgs

--- native.js
+++ native.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // TODO load this from elm runtime
 function F2(fun)
 {
@@ -6,6 +8,14 @@ function F2(fun)
   wrapper.func = fun;
   return wrapper;
 }
+
+const base64encode = data => Buffer.from(data).toString('base64');
+const base64decode = base64 => Buffer.from(base64, 'base64').toString('utf8');
+
+const stream = fs.createWriteStream("log.txt");
+stream.once('open', () => null);
+
+process.on('exit', () => stream.end());
 
 // inject globals for elm
 global._user$project$Native_Persistent = {
@@ -16,6 +26,8 @@ global._user$project$Native_Persistent = {
   wrapUpdate(f) {
     const g = (msg, model) => {
       console.log("updating", msg, model);
+      stream.write(base64encode(JSON.stringify(msg)));
+      stream.write('\n');
       return f(msg)(model)
     }
     return F2(g)

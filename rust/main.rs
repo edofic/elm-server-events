47228beac2b03a84106a551cb0a63ec43d490b86
Andraz Bajt <andraz@bajt.me>
2018-05-28 08:53:29 +0200

    [rust] Add state

--- rust/main.rs
+++ rust/main.rs
@@ -1,15 +1,31 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{http, server, App, HttpRequest, Responder, State};
+use std::sync::{Arc, Mutex};
 
-fn index(_info: HttpRequest) -> impl Responder {
+#[derive(Clone)]
+struct AppState {
+    counter: Arc<Mutex<i32>>,
+}
+
+fn index(_info: HttpRequest<AppState>) -> impl Responder {
     format!("Hello")
 }
 
+fn orderbook(state: State<AppState>) -> String {
+    let mut current = state.counter.lock().unwrap();
+    *current += 1;
+    format!("hello {}", current)
+}
+
 fn main() {
-    server::new(
-        || App::new()
-            .resource("/", |r| r.get().f(index)))
-        .bind("127.0.0.1:8080")
+    let initial_state = AppState {
+        counter: Arc::new(Mutex::new(0)),
+    };
+    server::new(move || {
+        App::with_state(initial_state.clone())
+            .resource("/", |r| r.get().f(index))
+            .route("/orderbook", http::Method::GET, orderbook)
+    }).bind("127.0.0.1:8080")
         .unwrap()
         .run()
 }

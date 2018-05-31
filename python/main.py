5955ad7df2c1240d2d58b079b3edd580df43bd7b
Andraz Bajt <andraz@bajt.me>
2018-05-31 07:39:14 +0200

    [python] Introduce an event loop

--- python/main.py
+++ python/main.py
@@ -5,7 +5,9 @@ from flask import Flask, Response
 
 MAX_ORDERBOOK_LEN = 100
 
-Order = collections.namedtuple('Order', 'userId, orderType, price')
+Order = collections.namedtuple('Order', 'user_id, order_type, price')
+
+PlaceOrder = collections.namedtuple('PlaceOrder', 'order')
 
 
 class OrderBook(collections.namedtuple('OrderBook', 'asks, bids')):
@@ -14,13 +16,13 @@ class OrderBook(collections.namedtuple('OrderBook', 'asks, bids')):
     def empty():
         return OrderBook((), ())
 
-    def placeOrder(self, userId, orderType, price):
-        assert isinstance(userId, int)
-        assert orderType in ('buy', 'sell')
-        assert isinstance(price, int)
-        order = Order(userId, orderType, price)
+    def place_order(self, order):
+        assert isinstance(order.user_id, int)
+        assert order.order_type in ('buy', 'sell')
+        assert isinstance(order.price, int)
+        order = Order(order.user_id, order.order_type, order.price)
         asks, bids = list(self.asks), list(self.bids)
-        if orderType == 'buy':
+        if order.order_type == 'buy':
             bids.append(order)
             bids.sort(key=lambda bid: bid.price)
             while len(bids) > MAX_ORDERBOOK_LEN:
@@ -31,21 +33,41 @@ class OrderBook(collections.namedtuple('OrderBook', 'asks, bids')):
             while len(asks) > MAX_ORDERBOOK_LEN:
                 bids.pop()
 
-        if bids and asks and bids[-1].price > asks[-1].price:
+        if bids and asks and bids[-1].price >= asks[-1].price:
             bids.pop()
             asks.pop()
 
         return OrderBook(tuple(asks), tuple(bids))
 
-    def toJSON(self):
+    def to_json(self):
         return json.dumps({
             'asks': self.asks,
             'bids': self.bids,
         })
 
 
+class State(object):
+
+    def __init__(self, initial_state, update_func):
+        self._state = initial_state
+        self._update_func = update_func
+
+    def snapshot(self):
+        return self._state
+
+    def dispatch(self, msg):
+        # TODO persistance
+        self._state = self._update_func(msg, self._state)
+
+
+def update(msg, state):
+    if isinstance(msg, PlaceOrder):
+        return state.place_order(msg.order)
+    else:
+        raise Exception("unknown message: " + msg)
+
 app = Flask(__name__)
-app.orderbook = OrderBook.empty()
+app.state = State(OrderBook.empty(), update)
 
 
 @app.route('/')
@@ -57,20 +79,20 @@ def hello():
 def orderbook():
     return Response(
         status=200,
-        response=app.orderbook.toJSON(),
+        response=app.state.snapshot().to_json(),
         mimetype='application/json',
     )
 
 
-@app.route('/buy/<int:userId>/<int:price>')
-def buy(userId, price):
-    app.orderbook = app.orderbook.placeOrder(userId, 'buy', price)
+@app.route('/buy/<int:user_id>/<int:price>')
+def buy(user_id, price):
+    app.state.dispatch(PlaceOrder(Order(user_id, 'buy', price)))
     return 'ok'
 
 
-@app.route('/sell/<int:userId>/<int:price>')
-def sell(userId, price):
-    app.orderbook = app.orderbook.placeOrder(userId, 'sell', price)
+@app.route('/sell/<int:user_id>/<int:price>')
+def sell(user_id, price):
+    app.state.dispatch(PlaceOrder(Order(user_id, 'sell', price)))
     return 'ok'
 
 

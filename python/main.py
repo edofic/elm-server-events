7c1fd90c656594a7f64db7609d4254c7597570dd
Andraz Bajt <andraz@bajt.me>
2018-05-31 08:15:34 +0200

    [python] Add persistance

--- python/main.py
+++ python/main.py
@@ -1,5 +1,8 @@
-import json
+import base64
 import collections
+import json
+import os.path
+import pickle
 
 from flask import Flask, Response
 
@@ -51,12 +54,28 @@ class State(object):
     def __init__(self, initial_state, update_func):
         self._state = initial_state
         self._update_func = update_func
+        if os.path.isfile('init.txt'):
+            with open('init.txt', 'rb') as init_file:
+                self._state = pickle.loads(
+                    base64.decodebytes(init_file.read()))
+                if os.path.isfile('log.txt'):
+                    with open('log.txt', 'rb') as log_file:
+                        for line in log_file.readlines():
+                            if not line:
+                                continue
+                            msg = pickle.loads(base64.decodebytes(line))
+                            self._state = update_func(msg, self._state)
+        else:
+            with open('init.txt', 'wb') as init_file:
+                init_file.write(base64.b64encode(pickle.dumps(self._state)))
+        self._log_file = open('log.txt', 'ab')
 
     def snapshot(self):
         return self._state
 
     def dispatch(self, msg):
-        # TODO persistance
+        self._log_file.write(base64.b64encode(pickle.dumps(msg)))
+        self._log_file.write(b'\n')
         self._state = self._update_func(msg, self._state)
 
 
@@ -64,7 +83,7 @@ def update(msg, state):
     if isinstance(msg, PlaceOrder):
         return state.place_order(msg.order)
     else:
-        raise Exception("unknown message: " + msg)
+        raise Exception("unknown message: " + str(msg))
 
 app = Flask(__name__)
 app.state = State(OrderBook.empty(), update)

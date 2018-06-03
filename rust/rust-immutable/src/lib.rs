cbb662093c67e153bbc59e45864b152aec2a4615
Andraz Bajt <andraz@bajt.me>
2018-06-03 18:33:25 +0200

    [rust-immutable] Copy rust mutable as skeleton

---
+++ rust/rust-immutable/src/lib.rs
@@ -0,0 +1,135 @@
+extern crate actix;
+extern crate serde;
+extern crate serde_json;
+
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::sync::{Arc, Mutex};
+
+use self::actix::Actor;
+
+pub trait EventSourced {
+    type Msg;
+    fn update(&mut self, msg: Self::Msg);
+}
+
+pub struct ManagedState<S> {
+    current_state: Arc<Mutex<S>>,
+    writer_addr: actix::Addr<actix::Syn, MsgWriter>,
+}
+
+// TODO why does the derived version not work?
+impl<S> Clone for ManagedState<S> {
+    fn clone(&self) -> ManagedState<S> {
+        ManagedState {
+            current_state: self.current_state.clone(),
+            writer_addr: self.writer_addr.clone(),
+        }
+    }
+}
+
+impl<'de, 's, S> ManagedState<S>
+where
+    S: EventSourced,
+    S: serde::Serialize,
+    S: serde::de::DeserializeOwned,
+    S::Msg: serde::Serialize,
+    S::Msg: serde::de::DeserializeOwned,
+{
+    pub fn new(initial_state: S) -> io::Result<ManagedState<S>> {
+        let state = Self::replay().or_else(|err| {
+            println!("error: {}", err);
+            Self::store_initial(initial_state)
+        })?;
+        let log_file = OpenOptions::new().write(true).append(true).open("log.txt")?;
+        let writer = MsgWriter { log_file };
+        let writer_addr: actix::Addr<actix::Syn, _> = writer.start();
+        Ok(ManagedState {
+            current_state: Arc::new(Mutex::new(state)),
+            writer_addr: writer_addr,
+        })
+    }
+
+    fn replay() -> io::Result<S> {
+        let mut init_file = File::open("init.txt")?;
+        let log_file = File::open("log.txt")?;
+
+        let mut init_string = String::new();
+        init_file.read_to_string(&mut init_string)?;
+
+        let mut state: S = serde_json::from_str(&init_string)?;
+
+        for line_res in BufReader::new(log_file).lines() {
+            let line = line_res?;
+            let msg: S::Msg = serde_json::from_str(&line)?;
+            state.update(msg)
+        }
+        println!("replay ok");
+
+        Result::Ok(state)
+    }
+
+    fn store_initial(initial_state: S) -> io::Result<S> {
+        let json_initial = serde_json::to_string(&initial_state)?;
+        let mut init_file = File::create("init.txt")?;
+        init_file.write_all(json_initial.as_bytes())?;
+        Result::Ok(initial_state)
+    }
+
+    pub fn with_snapshot<F, A>(&self, f: F) -> A
+    where
+        F: Fn(&S) -> A,
+    {
+        let current_state = &*self.current_state.lock().unwrap();
+        f(current_state)
+    }
+
+    pub fn dispatch(&self, msg: S::Msg) {
+        let json_msg = serde_json::to_string(&msg).unwrap();
+        let mut writer_msg = WriteMsg {
+            data: Box::new(json_msg),
+        };
+        let mut current_state = self.current_state.lock().unwrap();
+        // Yes this is a busy wait while holding a mutex but this will only
+        // block if the write buffer is full and the system is at capacity so
+        // it's acceptable
+        loop {
+            match self.writer_addr.try_send(writer_msg) {
+                Ok(_) => break,
+                Err(actix::prelude::SendError::Full(m)) => {
+                    writer_msg = m;
+                }
+                Err(actix::prelude::SendError::Closed(m)) => {
+                    writer_msg = m;
+                }
+            }
+        }
+        // TODO send msg to writer
+        current_state.update(msg);
+    }
+}
+
+struct WriteMsg {
+    data: Box<String>,
+}
+
+impl actix::Message for WriteMsg {
+    type Result = ();
+}
+
+struct MsgWriter {
+    log_file: File,
+}
+
+impl actix::Actor for MsgWriter {
+    type Context = actix::Context<Self>;
+}
+
+impl actix::Handler<WriteMsg> for MsgWriter {
+    type Result = ();
+    fn handle(&mut self, msg: WriteMsg, _ctx: &mut actix::Context<Self>) -> Self::Result {
+        self.log_file.write_all(msg.data.as_bytes()).unwrap();
+        self.log_file.write_all(b"\n").unwrap();
+    }
+}
---
+++ rust/rust-immutable/src/main.rs
@@ -0,0 +1,121 @@
+extern crate actix;
+extern crate actix_web;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+mod lib;
+
+use actix_web::{http, server, App, HttpRequest, HttpResponse, Path, Responder, State};
+
+use lib::{EventSourced, ManagedState};
+
+type AppState = ManagedState<Orderbook>;
+
+#[derive(Serialize, Deserialize)]
+struct Orderbook {
+    asks: Vec<Order>,
+    bids: Vec<Order>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Order {
+    user_id: UserId,
+    order_type: OrderType,
+    price: Price,
+}
+
+type UserId = u32;
+
+type Price = i32;
+
+impl Orderbook {
+    fn normalize(&mut self) {
+        self.asks.sort_unstable_by_key(|o| o.price);
+        self.bids.sort_unstable_by_key(|o| o.price);
+
+        self.asks.truncate(100);
+        self.bids.truncate(100);
+
+        if self.asks.len() == 0 || self.bids.len() == 0 {
+            return;
+        }
+
+        let bid_idx = self.bids.len() - 1;
+        if self.asks[0].price <= self.bids[bid_idx].price {
+            // match
+            self.asks.remove(0);
+            self.bids.remove(bid_idx);
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+enum OrderType {
+    Buy,
+    Sell,
+}
+
+impl EventSourced for Orderbook {
+    type Msg = Order;
+    fn update(&mut self, msg: Self::Msg) {
+        let order = msg;
+        match order.order_type {
+            OrderType::Buy => self.bids.push(order),
+            OrderType::Sell => self.asks.push(order),
+        };
+        self.normalize();
+    }
+}
+
+fn index(_info: HttpRequest<AppState>) -> impl Responder {
+    format!("Hello")
+}
+
+fn orderbook(state: State<AppState>) -> HttpResponse {
+    state.with_snapshot(|orderbook| HttpResponse::Ok().json(&orderbook))
+}
+
+fn place_bid(data: (State<AppState>, Path<(UserId, Price)>)) -> impl Responder {
+    let state = data.0;
+    let path = data.1;
+    let order = Order {
+        user_id: path.0,
+        order_type: OrderType::Buy,
+        price: path.1,
+    };
+    state.dispatch(order);
+    "ok"
+}
+
+fn place_ask(data: (State<AppState>, Path<(UserId, Price)>)) -> impl Responder {
+    let state = data.0;
+    let path = data.1;
+    let order = Order {
+        user_id: path.0,
+        order_type: OrderType::Sell,
+        price: path.1,
+    };
+    state.dispatch(order);
+    "ok"
+}
+
+fn main() {
+    let system = actix::System::new("main");
+    let initial_orderbook = Orderbook {
+        asks: Vec::new(),
+        bids: Vec::new(),
+    };
+    let initial_state = ManagedState::new(initial_orderbook).unwrap();
+    server::new(move || {
+        App::with_state(initial_state.clone())
+            .resource("/", |r| r.get().f(index))
+            .route("/orderbook", http::Method::GET, orderbook)
+            .route("/buy/{user}/{amount}", http::Method::GET, place_bid)
+            .route("/sell/{user}/{amount}", http::Method::GET, place_ask)
+    }).bind("127.0.0.1:8080")
+        .unwrap()
+        .start();
+    println!("Started http server: 127.0.0.1:8080");
+    system.run();
+}
